3bbfcd008340942a349826cc845c9affe1f47fcc
Muzammil <sharfmuzamil@gmail.com>
2026-09-18 14:36:24 +0500

    feat: modified fetching data according to ticker dictionary and saved in separate folders for different sectors

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -13,7 +13,9 @@ REQUEST_DELAY_SECONDS = 2
 
 # Ensure that the cache directory exists.
 def ensure_cache_directory_exists() -> None:
-    os.makedirs("data/cache", exist_ok=True)
+    for key in tickers.TICKERS:
+        sector = key
+        os.makedirs(f"data/cache/{sector}", exist_ok=True)
 
 # Fetch data for a single ticker.
 def fetch_ticker_data(symbol: str) -> dict:
@@ -29,22 +31,22 @@ def fetch_ticker_data(symbol: str) -> dict:
         return {"success": False, "symbol": symbol}
 
 # Save data for a single ticker to CSV.
-def save_ticker_data(symbol: str, data) -> None:
-    data.to_csv(f"data/cache/{symbol}.csv", index=False)
-    logging.info(f"Data for {symbol} saved to data/cache/{symbol}.csv with {len(data)} rows.")
+def save_ticker_data(symbol: str, sector: str, data) -> None:
+    data.to_csv(f"data/cache/{sector}/{symbol}.csv", index=False)
+    logging.info(f"Data for {symbol} saved to data/cache/{sector}/{symbol}.csv with {len(data)} rows.")
 
 # Fetch data for a single ticker with retry logic.
-def fetch_and_save_with_retry(symbol: str) -> bool:
+def fetch_and_save_with_retry(symbol: str, sector: str) -> bool:
     result = fetch_ticker_data(symbol)
     if result["success"]:
-        save_ticker_data(symbol, result["data"])
+        save_ticker_data(symbol,sector, result["data"])
         return True
     else:
         time.sleep(REQUEST_DELAY_SECONDS)
         logging.info(f"Retrying fetch for {symbol}...")
         retry_result = fetch_ticker_data(symbol)
         if retry_result["success"]:
-            save_ticker_data(symbol, retry_result["data"])
+            save_ticker_data(symbol,sector, retry_result["data"])
             return True
         else:
             logging.error(f"Failed to fetch data for {symbol} after retry.")
@@ -54,11 +56,14 @@ def fetch_and_save_with_retry(symbol: str) -> bool:
 def fetch_data() -> None:
     ensure_cache_directory_exists()
     failed_tickers: list = []
-    for ticker in tickers.TICKERS:
-        symbol = ticker["symbol"]
-        logging.info(f"Fetching data for {symbol}...")
-        if not fetch_and_save_with_retry(symbol):
-            failed_tickers.append(symbol)
+
+    for sector, companies in tickers.TICKERS.items():
+        logging.info(f"--- Processing sector: {sector} ({len(companies)} tickers) ---")
+
+        for symbol, name in companies.items():
+            logging.info(f"Fetching data for {symbol} ({name}) - {sector}...")
+            if not fetch_and_save_with_retry(symbol,sector):
+                failed_tickers.append(symbol)
 
     if failed_tickers:
         logging.warning(f"Failed to fetch data for the following tickers: {', '.join(failed_tickers)}")
